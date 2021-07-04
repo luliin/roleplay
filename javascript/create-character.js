5ebe49d3163829811c7862e0ee5063957f13315a
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-07-04 22:21:37 +0200

    Make sure not posting to playgroup when it is null

--- javascript/create-character.js
+++ javascript/create-character.js
@@ -24,7 +24,7 @@ const createCharacter = () => {
     origin: $("#character-origin").val(),
     background: $("#character-description").val(),
     milestones: $("#character-milestones").val(),
-    savedLevel: false
+    savedLevel: false,
   };
   axios
     .post(addCharacter + userEmail, newCharacter)
@@ -35,41 +35,54 @@ const createCharacter = () => {
           let saveToPlaygroup = JSON.parse(
             sessionStorage.getItem("saveToPlaygroup")
           );
-          swal(
-            "Lägga till i spelgrupp?",
-            "Vill du lägga till karaktären i " + saveToPlaygroup,
-            "info",
-            {
-              buttons: ["Nej", "Ja!"],
-            }
-          ).then(()=> {
-            let number = JSON.parse(sessionStorage.getItem("characterNumber"))
-            axios.post(
-              addCharacterToPlaygroup +
-                saveToPlaygroup +
-                "&userEmail="+userEmail+"&characterNumber=" + number
-            )
-            .then(resp => {
-              swal(
-                "Karaktären tillagd!",
-                "Karaktären har lagts till i " + resp.data.name,
-                "success"
-              ).then(() => {
-                $("#moves-tab").tab("show");
-              });
-            })
-            .catch(err => {
-              if(err.response) {
-                swal("Ett fel uppstod", err.response.data.message, "error")
-                .then(() => {
-                  $("#moves-tab").tab("show");
-                })
-              } else {
-                console.log(err);
-                $("#moves-tab").tab("show");
+          if (saveToPlaygroup) {
+            swal(
+              "Lägga till i spelgrupp?",
+              "Vill du lägga till karaktären i " + saveToPlaygroup,
+              "info",
+              {
+                buttons: ["Nej", "Ja!"],
               }
+            ).then(() => {
+              let number = JSON.parse(
+                sessionStorage.getItem("characterNumber")
+              );
+              axios
+                .post(
+                  addCharacterToPlaygroup +
+                    saveToPlaygroup +
+                    "&userEmail=" +
+                    userEmail +
+                    "&characterNumber=" +
+                    number
+                )
+                .then((resp) => {
+                  swal(
+                    "Karaktären tillagd!",
+                    "Karaktären har lagts till i " + resp.data.name,
+                    "success"
+                  ).then(() => {
+                    $("#moves-tab").tab("show");
+                  });
+                })
+                .catch((err) => {
+                  if (err.response) {
+                    swal(
+                      "Ett fel uppstod",
+                      err.response.data.message,
+                      "error"
+                    ).then(() => {
+                      $("#moves-tab").tab("show");
+                    });
+                  } else {
+                    console.log(err);
+                    $("#moves-tab").tab("show");
+                  }
+                });
             });
-          });
+          } else {
+            $("#moves-tab").tab("show");
+          }
         }
       );
     })
